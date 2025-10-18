e454aa2dca3722dec2fd0b890150b79c7ef233e9
Juriku <juriku@company.com>
2025-10-18 14:59:05 -0500

    init

---
+++ game.js
@@ -0,0 +1,136 @@
+/* global Phaser */
+
+import { createAnimations } from "./animations.js";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 256,
+  height: 244,
+  backgroundColor: "#000000",
+  parent: "game",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+
+new Phaser.Game(config);
+
+function preload() {
+  this.load.image("cloud1", "assets/scenery/overworld/cloud1.png");
+  this.load.image("floorbricks", "assets/scenery/overworld/floorbricks.png");
+  this.load.spritesheet("mario", "assets/entities/mario.png", {
+    frameWidth: 18,
+    frameHeight: 16,
+  });
+  this.load.audio("gameover", "assets/sound/music/gameover.mp3");
+  this.load.image("boss", "assets/entities/boss.png");
+}
+
+function create() {
+  this.add.image(100, 50, "cloud1").setOrigin(0, 0).setScale(0.15);
+
+  this.floor = this.physics.add.staticGroup();
+  this.floor
+    .create(0, config.height - 16, "floorbricks")
+    .setOrigin(0, 0.5)
+    .refreshBody();
+  this.floor
+    .create(150, config.height - 16, "floorbricks")
+    .setOrigin(0, 0.5)
+    .refreshBody();
+
+  this.mario = this.physics.add
+    .sprite(50, 100, "mario")
+    .setOrigin(0, 1)
+    .setCollideWorldBounds(true)
+    .setGravityY(300);
+
+  this.physics.world.setBounds(0, 0, 2000, config.height);
+  this.physics.add.collider(this.mario, this.floor);
+
+  this.cameras.main.setBounds(0, 0, 2000, config.height);
+  this.cameras.main.startFollow(this.mario);
+
+  createAnimations(this);
+
+  this.keys = this.input.keyboard.createCursorKeys();
+
+  this.boss = this.physics.add
+    .sprite(180, 100, "boss")
+    .setOrigin(0.5, 1)
+    .setCollideWorldBounds(true);
+
+  this.physics.add.collider(this.boss, this.floor);
+
+  this.physics.add.overlap(this.mario, this.boss, () => {
+    if (!this.mario.isDead) {
+      this.mario.isDead = true;
+      this.mario.anims.play("mario-dead");
+      this.mario.setCollideWorldBounds(false);
+      this.sound.add("gameover", { volume: 0.2 }).play();
+
+      setTimeout(() => {
+        this.mario.setVelocityY(-350);
+      }, 100);
+
+      setTimeout(() => {
+        this.scene.restart();
+      }, 2000);
+    }
+  });
+}
+
+function update() {
+  if (this.mario.isDead) return;
+
+  const bossSpeed = 40;
+
+  if (this.mario.x < this.boss.x) {
+    this.boss.setVelocityX(-bossSpeed);
+    this.boss.flipX = true;
+  } else if (this.mario.x > this.boss.x) {
+    this.boss.setVelocityX(bossSpeed);
+    this.boss.flipX = false;
+  }
+
+  if (this.keys.left.isDown) {
+    this.mario.anims.play("mario-walk", true);
+    this.mario.x -= 2;
+    this.mario.flipX = true;
+  } else if (this.keys.right.isDown) {
+    this.mario.anims.play("mario-walk", true);
+    this.mario.x += 2;
+    this.mario.flipX = false;
+  } else {
+    this.mario.anims.play("mario-idle", true);
+  }
+
+  if (this.keys.up.isDown && this.mario.body.touching.down) {
+    this.mario.setVelocityY(-300);
+    this.mario.anims.play("mario-jump", true);
+  }
+
+  if (this.mario.y >= config.height) {
+    this.mario.isDead = true;
+    this.mario.anims.play("mario-dead");
+    this.mario.setCollideWorldBounds(false);
+    this.sound.add("gameover", { volume: 0.2 }).play();
+
+    setTimeout(() => {
+      this.mario.setVelocityY(-350);
+    }, 100);
+
+    setTimeout(() => {
+      this.scene.restart();
+    }, 2000);
+  }
+}
